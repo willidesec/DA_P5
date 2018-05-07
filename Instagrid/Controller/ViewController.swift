27745ce45f1c6a8d10802bbd28b7802503259a73
William <william.desecot@hotmail.fr>
2018-05-07 12:38:45 +0200

    Modidification changing layout and adding UIImagePickerController

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,97 +8,100 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIGestureRecognizerDelegate {
 
     // MARK: - Outlet
     @IBOutlet weak var squareView: GridView!
+    @IBOutlet var layoutButtons: [UIButton]!
+    
+    // MARK: - Var
+    var tag: Int? = nil
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Square Shadow
-//        addShadow()
+        addShadow()
         
         // Image Corner
-        
+        squareView.addImageViewCorner()
     }
 
+    // A function to change the layout of the square view
+    @IBAction func changeLayout(_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            squareView.views[3].isHidden = false
+            squareView.views[1].isHidden = true
+            layoutButtons[0].isSelected = true
+            layoutButtons[1].isSelected = false
+            layoutButtons[2].isSelected = false
+        case 2:
+            squareView.views[1].isHidden = false
+            squareView.views[3].isHidden = true
+            layoutButtons[1].isSelected = true
+            layoutButtons[0].isSelected = false
+            layoutButtons[2].isSelected = false
+        case 3:
+            squareView.views[1].isHidden = false
+            squareView.views[3].isHidden = false
+            layoutButtons[2].isSelected = true
+            layoutButtons[1].isSelected = false
+            layoutButtons[0].isSelected = false
+        default:
+            break
+        }
+    }
+    
+   // A function to add a shadow to the Square
+    func addShadow() {
+        squareView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 50).cgColor
+        squareView.layer.shadowRadius = 4
+        squareView.layer.shadowOpacity = 0.5
+        squareView.layer.shadowOffset = CGSize(width: 0, height: 2)
+    }
 
+    // Add image from Library
+    @IBAction func importImage(_ sender: UIButton) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = false
+        self.present(imagePickerController, animated: true)
+        tag = sender.tag
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            guard let currentTag = tag else { return }
+            squareView.imageViews[currentTag - 4].contentMode = .scaleToFill
+            squareView.imageViews[currentTag - 4].image = image
+            squareView.addButtons[currentTag - 4].isHidden = true
+            
+            // Make the ImageView clickable
+            let UITapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tappedImage(_:)))
+            UITapRecognizer.delegate = self
+            squareView.imageViews[currentTag - 4].addGestureRecognizer(UITapRecognizer)
+            squareView.imageViews[currentTag - 4].isUserInteractionEnabled = true
+
+
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
     
+    @objc func tappedImage(_ sender: UIImageView) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = false
+        self.present(imagePickerController, animated: true)
+//        tag = sender.tag
+    }
     
-    
-    
-    // Layout Buttons
-//    @IBAction func changeLayout(_ sender: UIButton) {
-//        if sender.tag == 5 {
-//            squareView.imageView4.isHidden = false
-//            squareView.imageView2.isHidden = true
-//
-//        }
-//
-//        if sender.tag == 6 {
-//            squareView.imageView2.isHidden = false
-//            squareView.imageView4.isHidden = true
-//            layout2Button.setImage(#imageLiteral(resourceName: "Selected"), for: UIControlState.normal)
-//            if layout1Button.imageView != nil {
-//                layout1Button.setImage(nil, for: .normal)
-//            }
-//            if layout3Button.imageView != nil {
-//                layout3Button.setImage(nil, for: .normal)
-//            }
-//        }
-//
-//        if sender.tag == 7 {
-//            squareView.imageView2.isHidden = false
-//            squareView.imageView4.isHidden = false
-//            layout3Button.setImage(#imageLiteral(resourceName: "Selected"), for: UIControlState.normal)
-//            if layout2Button.imageView != nil {
-//                layout2Button.setImage(nil, for: .normal)
-//            }
-//            if layout1Button.imageView != nil {
-//                layout1Button.setImage(nil, for: .normal)
-//            }
-//        }
-//    }
-//
-//
-//    // A function to add a shadow to the Square
-//    func addShadow() {
-//        squareView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 50).cgColor
-//        squareView.layer.shadowRadius = 4
-//        squareView.layer.shadowOpacity = 0.5
-//        squareView.layer.shadowOffset = CGSize(width: 0, height: 2)
-//    }
-//
-//    // Add Image from librairy
-//    @IBAction func importImage(_ sender: UIButton) {
-//        let imagePickerController = UIImagePickerController()
-//        imagePickerController.delegate = self
-//        imagePickerController.sourceType = .photoLibrary
-//
-//        // En plus pour l'instant
-//        imagePickerController.allowsEditing = false
-//
-//        self.present(imagePickerController, animated: true)
-//
-//
-//
-//    }
-//
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//
-//        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
-//            squareView.imageView1.image = image
-//        } else {
-//            // Error message
-//        }
-//
-//        self.dismiss(animated: true, completion: nil)
-//    }
-//
-//
 }
 
--- Instagrid/View/GridView.swift
+++ Instagrid/View/GridView.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class GridView: UIView {
 
-    // Outlet
-    @IBOutlet var ImageViews: [UIImageView]!
+    // MARK: - Outlet
+    @IBOutlet var imageViews: [UIImageView]!
+    @IBOutlet var views: [UIView]!
+    @IBOutlet var addButtons: [UIButton]!
     
-//     A function do add Corner Radius to buttons
+    // A function do add Corner Radius to buttons
     func addImageViewCorner() {
         
-        for aImageView in ImageViews {
+        for aImageView in imageViews {
             aImageView.layer.cornerRadius = 2
         }
     }
