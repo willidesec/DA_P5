36a7fed144c4ed760d81c1a226fb3b9bd1e89ae3
William <william.desecot@hotmail.fr>
2018-06-06 17:33:06 +0200

    Adding documentation

--- Instagrid/Model/GridManager.swift
+++ Instagrid/Model/GridManager.swift
@@ -10,6 +10,12 @@ import UIKit
 import Foundation
 
 class GridManager {
+    /**
+     This is a method to convert the gridView into an image that can be share.
+     
+     - Parameter gridView: You have to add the gridView you want to be convert.
+     - Returns: A UIImage that can be share.
+     */
     static func convertGridViewToImage(gridView: GridView) -> UIImage? {
         UIGraphicsBeginImageContextWithOptions(gridView.bounds.size, gridView.isOpaque, 0.0)
         gridView.drawHierarchy(in: gridView.bounds, afterScreenUpdates: true)
